83e0ca019a28d9df774da4fbfbc40d2b255458b2
majudlorenzoni <juliamaria892@gmail.com>
2023-11-12 18:35:36 -0300

    Inserção da função deletar

--- js/script.js
+++ js/script.js
@@ -3,11 +3,51 @@ todoLists = document.querySelector(".todoLists"),
 pendingNum = document.querySelector(".pending-num"),
 clearButton = document.querySelector(".clear-button");
 
+function allTasks(){
+    let tasks = document.querySelectorAll(".pending");
+   pendingNum.textContent = tasks.length === 0 ? "no" : tasks.length;
+
+   let allLists = document.querySelectorAll(".list");
+   if(allLists.length > 0){
+      todoLists.style.marginTop = "20px";
+      clearButton.style.pointerEvents = "auto";
+      return;
+   }
+   todoLists.style.marginTop = "0px";
+   clearButton.style.pointerEvents = "none";
+}
+
 inputField.addEventListener("keyup", (e) => {
     let inputVal = inputField.value.trim();
     console.log(inputVal);
 
     if(e.key == "Enter" && inputVal.length> 0){
-    console.log("valid");
+        let liTag = ` <li class="list pending" onclick="handleStatus(this)">
+        <input type="checkbox" />
+        <span class="task">${inputVal}</span>
+        <i class="uil uil-trash" onclick="deleteTask(this)"></i>
+    </li>` ;
+
+        todoLists.insertAdjacentHTML("beforeend", liTag); 
+        inputField.value = "";
+        allTasks();
+
     }
 });
+
+function handleStatus(e){
+    const cheackbox = e.querySelector("input");
+    cheackbox.checked = cheackbox.checked ? false : true;
+    e.classList.toggle("pending");
+    allTasks();
+}
+
+clearButton.addEventListener("click", () => {
+    todoLists.innerHTML = "";
+    allTasks();
+});
+
+function deleteTask(e){
+    e.parentElement.remove();
+    allTasks();
+}
